bfab33b77b91601eb2a891e9c2009f954640432a
Roobon <roobon@gmail.com>
2024-02-18 13:07:46 +0600

    first commit

---
+++ resources/views/front.blade.php
@@ -0,0 +1,7 @@
+@extends ('app')
+
+@section('content')
+
+<div id="app"></div>
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- start banner Area -->
+    <div id="app"></div>
+
+@endsection
